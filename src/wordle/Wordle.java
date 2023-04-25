3bb3f5ae5f6fc3b127a73c23abe4d8f7bd06babd
EmaMass <emanuel.massuda@uabc.edu.mx>
2023-04-24 22:23:18 -0700

    Initial commit

---
+++ src/wordle/Wordle.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package wordle;
+
+import wordle.consola.TableroConsola;
+import wordle.consola.TecladoConsola;
+import wordle.holografico.TableroHolografico;
+import wordle.interfaces.ITablero;
+import wordle.interfaces.ITeclado;
+
+/**
+ *
+ * @author molguin
+ */
+public class Wordle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ITablero tablero = new TableroHolografico();
+        ITeclado teclado = new TecladoConsola();
+        JuegoWordle juego = new JuegoWordle(tablero, teclado);
+        BancoDePalabras banco = new BancoDePalabras();
+        //System.out.print(banco.size()); Para revisar tamaño del banco
+        String palabraClave = banco.palabraClave();
+        juego.jugar();
+    }
+    
+}
