64236c19af32b65b30945056076045f6fc34a3bd
EmaMass <emanuel.massuda@uabc.edu.mx>
2023-05-03 10:29:19 -0700

    Empezando con el branch grafico

--- src/main/java/wordle/holografico/TableroHolografico.java
+++ src/main/java/wordle/holografico/TableroHolografico.java
@@ -5,13 +5,19 @@
 package wordle.holografico;
 
 import wordle.interfaces.ITablero;
-
+import wordle.Canvas;
+import java.awt.Canvas;
 /**
  *
  * @author molguin
  */
 public class TableroHolografico implements ITablero {
-
+    private Canvas canvas;
+    public TableroHolografico(){
+        canvas = new Canvas("Juego de Wordle",500,400);
+        
+    }
+    
     @Override
     public String despliegaIntento(String palabra) {
         System.out.println("Despliegue Holografico de "+palabra);
